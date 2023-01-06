6ddf32bc6ed2ef860e344572e1c4389d8b8442e4
palisjam <pluckananuru@iom.int>
2023-01-06 12:07:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsBanking
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.txtIBAN = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.txtGivenName = new System.Windows.Forms.TextBox();
+            this.txtFamilyName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(160, 359);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(157, 29);
+            this.btnCreateAccount.TabIndex = 0;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // txtIBAN
+            // 
+            this.txtIBAN.Location = new System.Drawing.Point(35, 25);
+            this.txtIBAN.Name = "txtIBAN";
+            this.txtIBAN.ReadOnly = true;
+            this.txtIBAN.Size = new System.Drawing.Size(235, 27);
+            this.txtIBAN.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtEmailAddress);
+            this.groupBox1.Controls.Add(this.txtGivenName);
+            this.groupBox1.Controls.Add(this.txtFamilyName);
+            this.groupBox1.Location = new System.Drawing.Point(35, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 261);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Given Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Family Name";
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(125, 151);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(291, 27);
+            this.txtEmailAddress.TabIndex = 2;
+            // 
+            // txtGivenName
+            // 
+            this.txtGivenName.Location = new System.Drawing.Point(125, 100);
+            this.txtGivenName.Name = "txtGivenName";
+            this.txtGivenName.Size = new System.Drawing.Size(291, 27);
+            this.txtGivenName.TabIndex = 1;
+            // 
+            // txtFamilyName
+            // 
+            this.txtFamilyName.Location = new System.Drawing.Point(125, 49);
+            this.txtFamilyName.Name = "txtFamilyName";
+            this.txtFamilyName.Size = new System.Drawing.Size(291, 27);
+            this.txtFamilyName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtIBAN);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Name = "Form1";
+            this.Text = "New Banking";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnCreateAccount;
+        private TextBox txtIBAN;
+        private GroupBox groupBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox txtEmailAddress;
+        private TextBox txtGivenName;
+        private TextBox txtFamilyName;
+    }
+}
---
+++ IBAN/IBANController.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace IBAN
+{
+    public class IBANController
+    {
+        private WebDriverWait wait;
+        private IWebDriver driver;
+        public IBANController() {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments(new List<string>() {
+                "--silent-launch",
+                "--no-startup-window",
+                "no-sandbox",
+                "headless",});
+
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            driver = new ChromeDriver(chromeDriverService, chromeOptions);
+            driver.Url = "http://randomiban.com/?country=Netherlands";
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(0));
+        }
+        public string GetIBAN()
+        {
+            try
+            {                
+                IWebElement firstResult = wait.Until(e => e.FindElement(By.ClassName("ibandisplay")));
+                var gen_button = driver.FindElement(By.Id("gen_button"));
+                gen_button.Click();
+                return firstResult.Text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+    }
+}
